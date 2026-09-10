dcff1392447787c1e97ce3d87842821aa8fc5a34
Vaibhav Sijaria <vaibhav@sophic.dev>
2026-09-10 12:10:49 +0530

    Bump version to 0.3.2

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         pkgs: withChecks:
         pkgs.buildGo127Module {
           pname = "nmtui";
-          version = "0.3.1";
+          version = "0.3.2";
           src = self;
 
           vendorHash = "sha256-s0Tg4J8PCKIoJ8oA5/QDGRYRfOJ8dVcqLTuMSlDj958=";
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"nmtui/internal/ui"
 )
 
-var version = "0.3.1"
+var version = "0.3.2"
 
 func main() {
 	var (
